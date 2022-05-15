fb21aed1a8f58208f175511ed28c610c8151d6cd
tamaraeaton <teaton@banyanlabs.io>
2022-05-15 09:41:03 -0700

    formatted README

--- src/components/TVShowQuery/TVShowQuery.js
+++ src/components/TVShowQuery/TVShowQuery.js
@@ -2,22 +2,16 @@ import React, { useState } from 'react';
 import './TVShowQuery.css';
 
 function TVShowQuery() {
-    // use the state to show the list result and the search name
-    const [tvShowList, setTvShowList] = useState([]);
     const [subString, setSubString] = useState('');
-    const [showImage, setShowImage] = useState();
+    const [tvShowList, setTvShowList] = useState([]);
 
-    // create variable array to store the sorted list result
     const storeSortData = [];
 
     const getTVShowNames = () => {
 
-        // fetch the data using the query search name
         fetch('http://api.tvmaze.com/search/shows?q=' + subString)
             .then(response => response.json())
             .then(data => {
-                // put the data in an array and sort it
-                // use fa and fb to compare against the next data point
                 storeSortData.push(data.sort((a, b) => {
                     let fa = a.show.name.toLowerCase(),
                         fb = b.show.name.toLowerCase();
@@ -30,13 +24,8 @@ function TVShowQuery() {
                     return 0;
                 }
                 ))
-                // set the list and clear out the search name
-                // console.log(data.show.name)
-                // console.log(data?.show?.image?.medium)
-                // console.log(storeSortData.show.image?.medium)
                 console.log(data)
                 setTvShowList(data)
-                setShowImage(data)
                 setSubString('')
             })
             .catch(err => console.error(err));
@@ -49,47 +38,18 @@ function TVShowQuery() {
                 <input onChange={(event) => setSubString(event.target.value)} value={subString} placeholder="Search Name"></input>
                 <button onClick={() => getTVShowNames()}>Search</button>
             </div>
-        {tvShowList.map((item)=> {
-            return (
-                <div key={item.show.id}>
-                    <h3 >{item.show.name}</h3>
-                    
-                    {item.show.image?.medium &&
-                    <img src={item.show.image?.medium}></img>
-                    }
-                    <hr/>
-                </div>
-
-            )
-        })
-
-        }
-
-
-
-            {/* <table className="tvShowTable">
-                <thead>
-                    <tr className='tableHeader'>
-                        <th>TV Show Name</th>
-                        <th>TV Show Image</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tvShowList.map((search) => {
-                        return (
-
-                            <tr key={search.show.id}>
-                                <td className='tableData'>{search.show.name}</td>
-                                {search.show.image?.medium &&
-                                <td className='tableData'><img src={search.show.image?.medium} /></td>
-                                }
-                            </tr>
-                        )
-                    })
-                    }
-                </tbody>
-            </table> */}
-
+            {
+                tvShowList.map((item) => {
+                    return (
+                        item.show.image?.medium &&
+                        <div key={item.show.id}>
+                            <h3 >{item.show.name}</h3>
+                            <img src={item.show.image?.medium} alt='showImage'></img>
+                            <hr />
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
