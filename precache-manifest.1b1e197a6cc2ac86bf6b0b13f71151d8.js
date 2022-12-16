61da36cdadb3b3b98e6d3a2fa1ba9a3d72cdf33e
Tamara Eaton <Tamara.Eaton@bounteous.com>
2022-12-16 09:12:47 -0700

    Updates

---
+++ precache-manifest.1b1e197a6cc2ac86bf6b0b13f71151d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31096df36d52e9cd789aaba662ed5598",
+    "url": "/challenge/index.html"
+  },
+  {
+    "revision": "60c9a38d19b991cdb5f2",
+    "url": "/challenge/static/css/main.ebde6ad8.chunk.css"
+  },
+  {
+    "revision": "d4f767918a935cc399da",
+    "url": "/challenge/static/js/2.fc177258.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/challenge/static/js/2.fc177258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c9a38d19b991cdb5f2",
+    "url": "/challenge/static/js/main.ca333d64.chunk.js"
+  },
+  {
+    "revision": "e9b921364112b7cb6801",
+    "url": "/challenge/static/js/runtime-main.43cc6a31.js"
+  }
+]);
